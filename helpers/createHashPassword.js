ca936dd27c0f1fede9a36cdf76eda8433a7a94db
olgagaleta <olga.galeta.og@gmail.com>
2023-02-16 17:37:41 +0000

    add hw 4

---
+++ helpers/createHashPassword.js
@@ -0,0 +1,8 @@
+const bcrypt = require('bcryptjs')
+
+const createHashPassword = async (password) => {
+  const hashPassword = await bcrypt.hash(password, 10)
+  return hashPassword
+}
+
+module.exports = createHashPassword
